8f97adc4384e5416a5c252c5f5c4e7717d95a8e0
jakiboy98 <jakiboy98@gmail.com>
2015-09-24 08:27:24 -0600

    Popup Controlle

--- src/input/controller/InputRunner.java
+++ src/input/controller/InputRunner.java
@@ -1,5 +1,10 @@
 package input.controller;
 
-public class InputRunner {
-
+public class InputRunner 
+{
+	public static void main (String [] args)
+	{
+		PopupController myInput = new PopupController();
+		myInput.start();
+	}
 }
